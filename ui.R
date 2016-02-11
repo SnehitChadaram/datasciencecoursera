35d1c4b261332059c1234cdc462fb1eadad3c064
SnehitChadaram <snehitrocks@gmail.com>
2016-02-11 13:33:02 +0530

    Added Instructions

--- ui.R
+++ ui.R
@@ -14,7 +14,12 @@ shinyUI(
       h4('You entered weight as:'),
       verbatimTextOutput("owt"),
       h4('Your BMI is:'),
-      verbatimTextOutput("obmi")
+      verbatimTextOutput("obmi"),
+      h3('Instructions:'),
+      h4('Just enter your height in centimetres and weight in kilograms.
+         If you have trouble entering the values in these units, you can
+         use an external source for conversion.
+         After entering the values, just press the "Submit" button and wait for a moment.')
     )
   )
 )
